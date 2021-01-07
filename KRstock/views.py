3b867efc439cc8daad53eda408be185d40654f60
ksm0207 <ksm03071@naver.com>
2021-01-07 19:02:17 +0900

    파일관리

---
+++ KRstock/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(r):
+    return HttpResponse("Hello wolrd !!")
--- stockApp/Kstock/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class KstockConfig(AppConfig):
-    name = 'Kstock'
